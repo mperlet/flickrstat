d8f127159252449f998b8f0cbd170745a4c0d433
Mathias Perlet <mathias.perlet@igd-r.fraunhofer.de>
2014-10-22 16:51:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# FlickrStat Twitter Bot
+
+## Requirements
+
+    curl, jq, xargs, sed, bc, convert and t
+
+## Uses
+
+* [t](https://github.com/sferik/t)
+* [jq](https://github.com/stedolan/jq)
+* [flickrit](https://github.com/pboehm/flickrit)
+
+## Install on Fedora 20
+
+    sudo yum install ImageMagick jq rubygems ruby-devel gcc
+    sudo gem install t
+    t authorize -d
+    chmod +x flickrstats.sh
+    ./flickstats.sh
+
+## Screenshot
+
+![screenshot](http://api.drp.io/files/5447c7a508474.png "Screenshot")
+
---
+++ flickrstat.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Requires: curl, jq, xargs, sed, bc, convert and t
+
+# Flickr + Twitter User
+FUSER='mperlet'
+TUSER='@mperlet_'
+
+# User Info URL
+DATA=$(curl http://flickrit.pboehm.org/photos/$FUSER)
+
+# Data sum
+FAV=$(echo "Favorites...."  $(echo $DATA | jq ".[] .count_faves" | xargs | sed -e 's/\ /+/g' | bc))
+COM=$(echo "Comments....."  $(echo $DATA | jq ".[] .count_comments" | xargs | sed -e 's/\ /+/g' | bc))
+VIE=$(echo "Views........"  $(echo $DATA | jq ".[] .views" | xargs | sed -e 's/\ /+/g' | bc))
+COU=$(echo "sum(Images).."  $(echo $DATA | jq ".[] .count_faves" | wc -l))
+
+# Add Image
+convert -size 350x84 xc:whitesmoke -font Courier-Bold -pointsize 14 -fill black -draw "text 5,14 'Flickr stats for : $(echo $FUSER)'" -draw "text 5,38 '$(echo $VIE)'" -draw "text 5,52 '$(echo $FAV)'" -draw "text 5,66 '$(echo $COM)'" -draw "text 5,80 '$(echo $COU)'"  $FUSER.jpg
+
+# Twitter stuff
+t update -f $FUSER.jpg "$TUSER"
+
+# Delete File
+rm $FUSER.jpg
