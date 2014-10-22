5dd3b2acb38851fe2bb717231a89a4ae89fc88db
Mathias Perlet <mathias@mperlet.de>
2014-10-22 19:18:43 +0200

    new msg + png

--- flickrstat.sh
+++ flickrstat.sh
@@ -16,10 +16,10 @@ VIE=$(echo "Views........"  $(echo $DATA | jq ".[] .views" | xargs | sed -e 's/\
 COU=$(echo "sum(Images).."  $(echo $DATA | jq ".[] .count_faves" | wc -l))
 
 # Add Image
-convert -size 350x84 xc:whitesmoke -font Courier-Bold -pointsize 14 -fill black -draw "text 5,14 'Flickr stats for : $(echo $FUSER)'" -draw "text 5,38 '$(echo $VIE)'" -draw "text 5,52 '$(echo $FAV)'" -draw "text 5,66 '$(echo $COM)'" -draw "text 5,80 '$(echo $COU)'"  $FUSER.jpg
+convert -size 350x84 xc:whitesmoke -font Courier-Bold -pointsize 14 -fill black -draw "text 5,14 'Flickr stats for : $(echo $FUSER)'" -draw "text 5,38 '$(echo $VIE)'" -draw "text 5,52 '$(echo $FAV)'" -draw "text 5,66 '$(echo $COM)'" -draw "text 5,80 '$(echo $COU)'"  $FUSER.png
 
 # Twitter stuff
-t update -f $FUSER.jpg "$TUSER"
+t update -f $FUSER.png "Hey, $TUSER new stats!"
 
 # Delete File
-rm $FUSER.jpg
+rm $FUSER.png
